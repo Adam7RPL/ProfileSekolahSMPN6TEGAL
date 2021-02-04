8b405d1f104dcaab7120fbc8420be6c3fcb043b4
Adam7RPL <adamganteng598@gmail.com>
2021-02-04 08:35:56 +0700

    v1

--- app/src/main/java/com/example/profilesekolahsmpn6tegal/MainActivity.java
+++ app/src/main/java/com/example/profilesekolahsmpn6tegal/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.profilesekolahsmpn6tegal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.profilesekolahsmpn6tegal.HalamanBaruActivity;
+import com.example.profilesekolahsmpn6tegal.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+    private TextView eAttemptsInfo;
+
+    private String Username = "siswa";
+    private String Password = "siswa123";
+
+    boolean isValid = false;
+    private int counter = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnLogin);
+        eAttemptsInfo = findViewById(R.id.tvAttemptsInfo);
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                if(inputName.isEmpty() || inputPassword.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter all the details correctly!", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    isValid = validate(inputName, inputPassword);
+
+                    if(!isValid){
+
+                        counter--;
+
+                        Toast.makeText(MainActivity.this, "Incorrect credentials entered!", Toast.LENGTH_SHORT).show();
+
+                        eAttemptsInfo.setText("No. of attempts remaining: 5" + counter);
+
+                        if(counter ==0){
+                            eLogin.setEnabled(false);
+                        }
+                    }else {
+
+                        Toast.makeText(MainActivity.this, "Login successfull!", Toast.LENGTH_SHORT).show();
+
+                        //tambahkan kode untuk pergi ke aktivitas baru
+                        Intent intent = new Intent(MainActivity.this, HalamanBaruActivity.class);
+                        startActivity(intent);
+                    }
+
+                }
+
+            }
+        });
+    }
+
+    private boolean validate(String name,String password){
+        if(name.equals(Username)&& password.equals(Password)){
+            return true;
+        }
+
+        return false;
     }
 }
