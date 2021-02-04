caffeaed7103cb9fc40617a3e7c99dfdc9ee0036
nitanur08 <nitanuroktaviani301@gmail.com>
2021-02-04 11:25:39 +0700

    v2

--- app/src/main/java/com/example/profilesekolahsmpn6tegal/HalamanBaruActivity.java
+++ app/src/main/java/com/example/profilesekolahsmpn6tegal/HalamanBaruActivity.java
@@ -2,7 +2,9 @@ package com.example.profilesekolahsmpn6tegal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HalamanBaruActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class HalamanBaruActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_baru);
+        }
+    public void prestasi(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, PrestasiActivity.class);
+        startActivity(intent);
     }
+    public void profil(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+    public void informasi(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, InformasiActivity.class);
+        startActivity(intent);
+    }
+    public void tugas(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, TugasActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekstrakulikuler(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, EkstrakulikulerActivity.class);
+        startActivity(intent);
+    }
+    public void absen(View view){
+        Intent intent=new Intent(HalamanBaruActivity.this, AbsenActivity.class);
+        startActivity(intent);
+    }
+
+
 }
