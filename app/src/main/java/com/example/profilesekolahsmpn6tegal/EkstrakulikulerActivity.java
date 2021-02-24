1f3e67c5719f69c4b1cb27e92ac009b648aa00d9
DinangAm <dinangagusm3@gmail.com>
2021-02-24 14:03:55 +0700

    pekan5

--- app/src/main/java/com/example/profilesekolahsmpn6tegal/EkstrakulikulerActivity.java
+++ app/src/main/java/com/example/profilesekolahsmpn6tegal/EkstrakulikulerActivity.java
@@ -2,7 +2,9 @@ package com.example.profilesekolahsmpn6tegal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EkstrakulikulerActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class EkstrakulikulerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekstrakulikuler);
+    }
+    public void futsal(View view){
+        Intent intent=new Intent(EkstrakulikulerActivity.this, FutsalActivity.class);
+        startActivity(intent);
+    }
+    public void volly(View view){
+        Intent intent=new Intent(EkstrakulikulerActivity.this, VollyActivity.class);
+        startActivity(intent);
+    }
+    public void basket(View view){
+        Intent intent=new Intent(EkstrakulikulerActivity.this, BasketActivity.class);
+        startActivity(intent);
+    }
+    public void marchingband(View view){
+        Intent intent=new Intent(EkstrakulikulerActivity.this, MarchingbandActivity.class);
+        startActivity(intent);
+    }
+    public void silat(View view){
+        Intent intent=new Intent(EkstrakulikulerActivity.this, SilatActivity.class);
+        startActivity(intent);
     }
 }
