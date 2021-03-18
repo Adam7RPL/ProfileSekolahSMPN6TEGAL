bb8a21ffeb2c446ee1b1ea4de8d34ab721c1a036
DinangAm <dinangagusm3@gmail.com>
2021-03-18 12:29:12 +0700

    menambah menu dlam jadwal

---
+++ app/src/main/java/com/example/profilesekolahsmpn6tegal/Kelas7Activity.java
@@ -0,0 +1,14 @@
+package com.example.profilesekolahsmpn6tegal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kelas7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelas7);
+    }
+}
